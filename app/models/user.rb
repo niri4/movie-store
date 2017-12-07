48d9816c90767ed5febe3b39f08b5a40b4dcf0f7
niri4 <nirankarsingh247@gmail.com>
2017-12-08 00:47:51 +0530

    add name and image in user model and white list param

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_many :comments
-         has_many :movies, through: :comments
+  has_many :comments
+  has_many :movies, through: :comments
+  validates_presence_of :name
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207082002) do
+ActiveRecord::Schema.define(version: 20171207185717) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "body"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20171207082002) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
