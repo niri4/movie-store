69c216b6a17e38d32d209b99fb73aae02bbe9bac
niri-maro <nirankar@maropost.com>
2017-12-08 14:51:07 +0530

    Implemented logic for user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:image])
   end
 end
