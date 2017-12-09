66dfe6cff7758f43b60326ebbba6b2b157ec4c50
niri4 <nirankarsingh247@gmail.com>
2017-12-10 01:40:08 +0530

    add dependentt destroy to has_many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :movies, through: :comments
   validates_presence_of :name
 
