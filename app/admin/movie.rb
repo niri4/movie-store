26bc0ae71dc418a73d57a7f5208f2faa90b6ebaa
niri4 <nirankarsingh247@gmail.com>
2017-12-10 05:02:03 +0530

    Implemeted movie create logic

---
+++ app/admin/movie.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Movie do
+  permit_params :title,:image,:genre,:plot,:rating,:web,:year,:cast,:view
+  decorate_with UserDecorator
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :image
+    column :genre
+    column :plot
+    column :rating
+    actions
+  end
+
+  filter :title
+  filter :genre
+
+  action_item :new_movie,only: :index do
+    link_to "Automatically Movie Upload", "http://localhost:3000/admin/movies/new?view=automatic"
+  end
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs do
+      render "movies/form",{movies: movie,view: params[:view]}
+    end
+  end
+
+
+
+end
