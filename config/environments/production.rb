c9169357d0bd83139e7038ee88ae9e1c18399635
niri-maro <nirankar@maropost.com>
2017-12-11 19:06:30 +0530

    make dummy file ignore it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "movie-store_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      # enable_starttls_auto: true,
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: 'plain',
+      user_name: Rails.application.secrets.email,
+      password: Rails.application.secrets.password
+    }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
